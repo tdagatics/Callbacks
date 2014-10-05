b49ffb8fe4e6bef583c3f3a5bc43c1c845a83d4c
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-05 10:14:53 -0700

    implemented observalueForKeyPath

--- Callbacks/BNRObserver.m
+++ Callbacks/BNRObserver.m
@@ -10,4 +10,12 @@
 
 @implementation BNRObserver
 
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    NSString *oldValue = [change objectForKey:NSKeyValueChangeOldKey];
+    NSString *newValue = [change objectForKey:NSKeyValueChangeNewKey];
+    NSLog(@"Observed %@ of %@ was changed from %@ to %@", keyPath, object, oldValue, newValue);
+    
+}
+
 @end
