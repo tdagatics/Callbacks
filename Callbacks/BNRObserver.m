339d6a8ca682c92119f8a504eabdec79cb3fd3b5
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-05 10:11:35 -0700

    Added new class and added observer to logger for key path lastTime

---
+++ Callbacks/BNRObserver.m
@@ -0,0 +1,13 @@
+//
+//  BNRObserver.m
+//  Callbacks
+//
+//  Created by Anthony Dagati on 10/5/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import "BNRObserver.h"
+
+@implementation BNRObserver
+
+@end
--- Callbacks/main.m
+++ Callbacks/main.m
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import "BNRLogger.h"
+#import "BNRObserver.h"
 
 int main(int argc, const char * argv[])
 {
@@ -27,6 +28,11 @@ int main(int argc, const char * argv[])
         __unused NSURLConnection *fetchConn = [[NSURLConnection alloc] initWithRequest:request delegate:logger startImmediately:YES];
         
         __unused NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:2.0 target:logger selector:@selector(updateLastTime:) userInfo:nil repeats:YES];
+        
+        __unused BNRObserver *observer = [[BNRObserver alloc] init];
+        
+        [logger addObserver:observer forKeyPath:@"lastTime" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
+        
         
         [[NSRunLoop currentRunLoop] run];
         
